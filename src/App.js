b1223f1d8a8202a289090c86efba04a821fc7eca
iSrg <mdb4@njit.edu>
2024-05-13 01:20:35 -0400

    not working backend connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-function App() {
-  return (
-    <div className="App" style={{background: 'black'}}>
+import axios from 'axios';
+import React, { useState } from 'react';
 
+function App() {
+  const [data, setData] = useState('Nahhhh');
+  const get_hello_world = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/hello');
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  get_hello_world();
+  return (
+    <div>
+      <p>{data}</p>
     </div>
   );
 }
